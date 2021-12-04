755d5797e7d62c0d32de5cd7cf197bf419353e8b
Chris Joubert <omega@gamerlobby.net>
2021-12-04 17:55:51 +0200

    updated template
    Updated template

--- demo.py
+++ demo.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
-
 def home():
     return render_template("index.html")
+
+# adds path to the hosted page (localhost7000/mech)
+@app.route("/mech", methods = ["GET"])
+def mech():
+    return render_template("mech.html")
+
 
 
 if __name__ == "__main__":
